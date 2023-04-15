f6ba301239ac2d90dee604a68fe5f7361400db23
Denys Strashniuk <strashnyuk.denis@gmail.com>
2023-04-15 15:15:58 +0200

    add task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const ingredientEl = ingredients.map(el => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = el;
+  itemEl.classList.add('item');
+
+  return itemEl;
+});
+
+ingredientsEl.append(...ingredientEl);
