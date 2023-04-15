2568720d52ba7141f682834fd641d2cb0941b3d8
Denys Strashniuk <strashnyuk.denis@gmail.com>
2023-04-15 19:08:12 +0200

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const valueColorEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  valueColorEl.textContent = randomColor;
+});
