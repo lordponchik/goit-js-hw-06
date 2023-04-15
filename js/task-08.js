48f14966ed47a4a8bcd779b66a359060abcf8580
Denys Strashniuk <strashnyuk.denis@gmail.com>
2023-04-15 19:02:33 +0200

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (email.value.length === 0 || password.value.length === 0) {
+    alert('Внимание! Все поля должны быть заполнены.');
+  } else {
+    const dataForm = {
+      email: email.value,
+      password: password.value,
+    };
+
+    console.log(dataForm);
+
+    event.currentTarget.reset();
+  }
+});
