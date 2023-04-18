79af14f3c7c4d32135137021af7416439a629b0a
Denys Strashniuk <strashnyuk.denis@gmail.com>
2023-04-18 11:23:20 +0200

    fix task-5 inputEl.addEventListener

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,9 @@ const inputEl = document.querySelector('#name-input');
 const outputNameEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', event => {
-  outputNameEl.textContent = event.currentTarget.value;
-
-  if (event.currentTarget.value === '') {
+  if (event.currentTarget.value[0] === ' ' || event.currentTarget.value === '') {
     outputNameEl.textContent = 'Anonymous';
+  } else {
+    outputNameEl.textContent = event.currentTarget.value;
   }
 });
