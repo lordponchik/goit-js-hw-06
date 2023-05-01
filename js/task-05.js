ca59e1e7b72a24f1d3004211f404399a74dc06bd
Denys Strashniuk <denysstrashniuk@MacBook-Pro-Denys-Strashniuk.local>
2023-05-01 17:05:06 +0200

    change task-05

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,6 @@ const outputNameEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', event => {
   const valueInput = event.currentTarget.value;
-  if (valueInput.trim().length !== 0) {
-    outputNameEl.textContent = valueInput.trim();
-  } else if (valueInput.length === 0 || valueInput[0] === ' ') {
-    outputNameEl.textContent = 'Anonymous';
-  }
+
+  outputNameEl.textContent = valueInput.trim().length !== 0 ? valueInput.trim() : 'Anonymous';
 });
